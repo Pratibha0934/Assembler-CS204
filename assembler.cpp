78329350780eb1bf86d8ea448a6d80303a0408a1
shivambhagat79 <shivambhagat2279+github@gmail.com>
2024-03-07 05:28:47 +0530

    Finish assembling R format commands.

--- assembler.cpp
+++ assembler.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <bitset>
+#include <bits/stdc++.h>
 #include <fstream>
+#include "static.cpp"
 #include "utilities.cpp"
 
 using namespace std;
 
-vector<string> input, output;
+vector<string> dataLines, textLines, outputLines;
 unordered_map<string, long long> labels;
 
 void preProcessInput(string fileName)
@@ -16,26 +16,57 @@ void preProcessInput(string fileName)
     string inp, label;
     long long pc = 0;
 
+    bool isData = false;
+    bool isText = true;
+
     if (file.is_open())
     {
         while (getline(file, inp))
         {
+            trim(inp);
 
-            size_t colonPos = inp.find(':');
-            if (colonPos != string::npos)
+            if (inp == "")
             {
-                label = inp.substr(0, colonPos);
-                trim(label);
-
-                inp = inp.substr(colonPos + 1);
-                trim(inp);
-
-                pair<string, long long> newLabel(label, pc);
-                labels[label] = pc;
+                continue;
             }
 
-            input.push_back(inp);
-            pc += 4;
+            if (inp == ".data")
+            {
+                isData = true;
+                isText = false;
+                continue;
+            }
+            if (inp == ".text")
+            {
+                isData = false;
+                isText = true;
+                continue;
+            }
+
+            rectifyCode(inp);
+
+            if (isData)
+            {
+                dataLines.push_back(inp);
+            }
+
+            if (isText)
+            {
+                size_t colonPos = inp.find(':');
+                if (colonPos != string::npos)
+                {
+                    label = inp.substr(0, colonPos);
+                    trim(label);
+
+                    inp = inp.substr(colonPos + 1);
+                    trim(inp);
+
+                    labels[label] = pc;
+                }
+
+                textLines.push_back(inp);
+                pc += 4;
+            }
         }
     }
     else
@@ -48,8 +79,94 @@ void preProcessInput(string fileName)
     file.close();
 }
 
+void assemble_R(string code, long long pc)
+{
+    vector<string> words = split(code);
+    string rs1, rs2, rd, opcode, func3, func7;
+
+    opcode = instructionData[words[0]]["opcode"];
+    func3 = instructionData[words[0]]["func3"];
+    func7 = instructionData[words[0]]["func7"];
+
+    rd = regToStr[words[1]];
+    rs1 = regToStr[words[2]];
+    rs2 = regToStr[words[3]];
+
+    string bin = func7 + rs2 + rs1 + func3 + rd + opcode;
+    string output = decToHex(pc) + " " + binToHex(bin);
+
+    outputLines.push_back(output);
+}
+
+void assembleText()
+{
+    string code, op;
+    long long pc;
+
+    for (size_t i = 0; i < textLines.size(); i++)
+    {
+        code = textLines[i];
+        pc = i * 4;
+
+        int spacePos = code.find(' ');
+        if (spacePos != string::npos)
+        {
+            op = code.substr(0, spacePos);
+
+            if (R.find(op) != R.end())
+            {
+                assemble_R(code, pc);
+            }
+            else if (I.find(op) != I.end())
+            {
+            }
+            else if (S.find(op) != S.end())
+            {
+            }
+            else if (SB.find(op) != SB.end())
+            {
+            }
+            else if (U.find(op) != U.end())
+            {
+            }
+            else if (UJ.find(op) != UJ.end())
+            {
+            }
+            else
+            {
+                cout << "Error at line " << i << " : " << code << endl;
+                cout << "Exiting program ." << endl;
+                exit(-1);
+            }
+        }
+        else
+        {
+            cout << "Error at line " << i << " : " << code << endl;
+            cout << "Exiting program ." << endl;
+            exit(-1);
+        }
+    }
+}
+
 int main()
 {
-    preProcessInput("input.asm");
+    initialiseStaticData();
+    preProcessInput("sample.asm");
+    assembleText();
+    // for (auto x : dataLines)
+    // {
+    //     cout << x << endl;
+    // }
+
+    // cout << "/////" << endl;
+
+    // for (auto x : textLines)
+    // {
+    //     cout << x << endl;
+    // }
+    for (auto x : outputLines)
+    {
+        cout << x << endl;
+    }
     return 0;
 }
--- utilities.cpp
+++ utilities.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
+#include <sstream>
 
 using namespace std;
 
+// String Utility Functions //////////////////////////////////////////////////////////////////
 // trim from start
 inline void ltrim(string &s)
 {
@@ -24,3 +26,121 @@ inline void trim(string &s)
     rtrim(s);
     ltrim(s);
 }
+
+// Remove commas and consecutive spaces
+inline void rectifyCode(string &s)
+{
+    s.erase(remove(s.begin(), s.end(), ','), s.end());
+    s.erase(std::unique(s.begin(), s.end(), [](char a, char b)
+                        { return a == ' ' && b == ' '; }),
+            s.end());
+}
+
+// Split a string into words
+vector<string> split(string s)
+{
+    vector<string> output;
+    string word;
+
+    istringstream iss(s);
+
+    while (iss >> word)
+    {
+        output.push_back(word);
+    }
+
+    return output;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Number Base Conversion functions /////////////////////////////////////////////////////////
+
+// Create map between binary number and its equivalent hexadecimal
+void createMap(unordered_map<string, char> *um)
+{
+    (*um)["0000"] = '0';
+    (*um)["0001"] = '1';
+    (*um)["0010"] = '2';
+    (*um)["0011"] = '3';
+    (*um)["0100"] = '4';
+    (*um)["0101"] = '5';
+    (*um)["0110"] = '6';
+    (*um)["0111"] = '7';
+    (*um)["1000"] = '8';
+    (*um)["1001"] = '9';
+    (*um)["1010"] = 'A';
+    (*um)["1011"] = 'B';
+    (*um)["1100"] = 'C';
+    (*um)["1101"] = 'D';
+    (*um)["1110"] = 'E';
+    (*um)["1111"] = 'F';
+}
+
+// convert binary to hexadecimal
+string binToHex(string bin)
+{
+    string hex = "0x";
+    int val;
+    char ch;
+
+    for (int i = 0; i <= 28; i += 4)
+    {
+        val = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            if (bin[i + j] == '1')
+            {
+                val += pow(2, 3 - j);
+            }
+        }
+
+        if (val <= 9)
+        {
+            ch = 48 + val;
+        }
+        else
+        {
+            ch = 55 + val;
+        }
+
+        hex += ch;
+    }
+
+    return hex;
+}
+
+// convert decimal to hexadecimal
+string decToHex(long long dec)
+{
+    string hex = "";
+
+    int rem;
+    char ch;
+
+    while (dec != 0)
+    {
+        rem = dec % 16;
+
+        if (rem < 10)
+        {
+            ch = rem + 48;
+        }
+        else
+        {
+            ch = rem + 55;
+        }
+
+        hex += ch;
+        dec = dec / 16;
+    }
+
+    if (hex == "")
+    {
+        hex += '0';
+    }
+
+    reverse(hex.begin(), hex.end());
+
+    return ("0x" + hex);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
